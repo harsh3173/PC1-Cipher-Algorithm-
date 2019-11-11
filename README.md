f2f489ce7908cdfbd0cbbad0a14d99264228e4be
Harsh3211 <42844610+Harsh3211@users.noreply.github.com>
2019-11-12 01:34:58 +0530

    1st Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+PC1 Encryption/Decryption Program
+PC1 Cipher 256 bits
+
+
+  Create encryption & decryption keys:
+
+
+  PC1 Encryption:
+	./en (name of file to be encrypted) (name of file where encrypted text should be stored)
+	Enter a password upto 32 charecters 
+	Split the 'c' crypted byte into two 4 bits parts 'd' and 'e' 
+  
+  Pc1 Decryption:
+	./de (name of encrypted file) (name of file where decrypted text should be stored)  
+	Enter the password (upto 32 charecters) 
+
---
+++ pc1cod.c
@@ -0,0 +1,221 @@
+﻿// Encryption
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+unsigned short ax,bx,cx,dx,si,tmp,x1a2,x1a0[16],res,i,inter,cfc,cfd,compte;
+unsigned char cle[32]; 
+unsigned char buff[32];
+short c;
+int c1,count;
+short d,e;
+FILE *in,*out;
+
+void fin() {
+// erase all the variables
+
+    for (compte=0; compte<=31; compte++) {
+        cle[compte]=0;
+    }
+    ax=0;
+    bx=0;
+    cx=0;
+    dx=0;
+    si=0;
+    tmp=0;
+    x1a2=0;
+    x1a0[0]=0;
+    x1a0[1]=0;
+    x1a0[2]=0;
+    x1a0[3]=0;
+    x1a0[4]=0;
+    res=0;
+    i=0;
+    inter=0;
+    cfc=0;
+    cfd=0;
+    compte=0;
+    c=0;
+}
+
+void assemble() {
+
+    x1a0[0]= ( cle[0]*256 )+ cle[1];
+    code();
+    inter=res;
+
+    x1a0[1]= x1a0[0] ^ ( (cle[2]*256) + cle[3] );
+    code();
+    inter=inter^res;
+
+    x1a0[2]= x1a0[1] ^ ( (cle[4]*256) + cle[5] );
+    code();
+    inter=inter^res;
+
+    x1a0[3]= x1a0[2] ^ ( (cle[6]*256) + cle[7] );
+    code();
+    inter=inter^res;
+
+    x1a0[4]= x1a0[3] ^ ( (cle[8]*256) + cle[9] );
+    code();
+    inter=inter^res;
+
+    x1a0[5]= x1a0[4] ^ ( (cle[10]*256) + cle[11] );
+    code();
+    inter=inter^res;
+
+    x1a0[6]= x1a0[5] ^ ( (cle[12]*256) + cle[13] );
+    code();
+    inter=inter^res;
+
+    x1a0[7]= x1a0[6] ^ ( (cle[14]*256) + cle[15] );
+    code();
+    inter=inter^res;
+
+    x1a0[8]= x1a0[7] ^ ( (cle[16]*256) + cle[17] );
+    code();
+    inter=inter^res;
+
+    x1a0[9]= x1a0[8] ^ ( (cle[18]*256) + cle[19] );
+    code();
+    inter=inter^res;
+
+    x1a0[10]= x1a0[9] ^ ( (cle[20]*256) + cle[21] );
+    code();
+    inter=inter^res;
+
+    x1a0[11]= x1a0[10] ^ ( (cle[22]*256) + cle[23] );
+    code();
+    inter=inter^res;
+
+    x1a0[12]= x1a0[11] ^ ( (cle[24]*256) + cle[25] );
+    code();
+    inter=inter^res;
+
+    x1a0[13]= x1a0[12] ^ ( (cle[26]*256) + cle[27] );
+    code();
+    inter=inter^res;
+
+    x1a0[14]= x1a0[13] ^ ( (cle[28]*256) + cle[29] );
+    code();
+    inter=inter^res;
+
+    x1a0[15]= x1a0[14] ^ ( (cle[30]*256) + cle[31] );
+    code();
+    inter=inter^res;
+
+    i=0;
+}
+
+int code() {
+    dx=x1a2+i;
+    ax=x1a0[i];
+    cx=0x015a;
+    bx=0x4e35;
+
+    tmp=ax;
+    ax=si;
+    si=tmp;
+
+    tmp=ax;
+    ax=dx;
+    dx=tmp;
+
+    if (ax!=0) {
+        ax=ax*bx;
+    }
+
+    tmp=ax;
+    ax=cx;
+    cx=tmp;
+
+    if (ax!=0) {
+        ax=ax*si;
+        cx=ax+cx;
+    }
+
+    tmp=ax;
+    ax=si;
+    si=tmp;
+    ax=ax*bx;
+    dx=cx+dx;
+
+    ax=ax+1;
+
+    x1a2=dx;
+    x1a0[i]=ax;
+
+    res=ax^dx;
+//    i=i+1;
+    return 0;
+}
+
+int main(int argc,char* argv[]) {
+    si=0;
+    x1a2=0;
+    i=0;
+
+    if(argc!=3) {
+        errno=EINVAL;
+        perror("Bad Arguments");
+        return errno;
+    }
+
+    // "abcdefghijklmnopqrstuvwxyz01234" is the default password used
+    // if the user enter a key < 32 characters, characters of the default password will be used 
+
+    strcpy(cle,"abcdefghijklmnopqrstuvwxyz01234");
+
+    printf ("PC1 Cipher 256 bits \nENCRYPT file\n");
+    printf("Enter a password upto 32 charecters:");
+    scanf("%s",buff);
+
+    if (strlen(buff)>32) {
+        count=32;
+    } else {
+        count=strlen(buff);
+    }
+
+    for (c1=0; c1<count; c1++) {
+        cle[c1]=buff[c1];
+    }
+
+    if ((in=fopen(argv[1],"rb")) == NULL) {
+        printf("\nError reading file!\n");
+        fin();
+    }
+
+    if ((out=fopen(argv[2],"wb")) == NULL) {
+        printf("\nError writing file!\n");
+        fin();
+    }
+
+    // c contains the byte read in the file 
+    while ( (c=fgetc(in)) != EOF) {
+
+        assemble();
+        cfc=inter>>8;
+        cfd=inter&255; // cfc^cfd = random byte 
+
+        for (compte=0; compte<=31; compte++)
+        {
+            // we mix the plaintext byte with the key 
+
+            cle[compte]=cle[compte]^c;
+        }
+
+        c = c ^ (cfc^cfd);
+
+
+        d=(c >> 4); // we split the 'c' crypted byte into two 4 bits parts 'd' and 'e' 
+        e=(c & 15);
+
+        fputc(0x25+d,out); //we write the two 4 bits parts as ASCII letters 
+        fputc(0x25+e,out);
+
+    }
+    fclose (in);
+    fclose (out);
+    fin();
+    return(0);
+}
+
